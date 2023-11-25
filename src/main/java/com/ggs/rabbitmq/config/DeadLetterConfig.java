bc4062c101b06fde070b825a232f2b7352ed1e2f
lianghaohui <1285226024@qq.com>
2023-11-25 18:00:24 +0800

    feat:死信队列

---
+++ src/main/java/com/ggs/rabbitmq/config/DeadLetterConfig.java
@@ -0,0 +1,63 @@
+package com.ggs.rabbitmq.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author starbug
+ * @Description
+ * @Datetime 2023/11/25 17:55
+ */
+@Configuration
+public class DeadLetterConfig {
+
+    public static final String NORMAL_EXCHANGE = "normal-exchange";
+    public static final String NORMAL_QUEUE = "normal-queue";
+    public static final String NORMAL_ROUTING_KEY = "normal.#";
+
+    public static final String DEAD_EXCHANGE = "dead-exchange";
+    public static final String DEAD_QUEUE = "dead-queue";
+    public static final String DEAD_ROUTING_KEY = "dead.#";
+
+    @Bean
+    public TopicExchange normalExchange() {
+        return ExchangeBuilder
+                .topicExchange(NORMAL_EXCHANGE)
+                .build();
+    }
+
+    @Bean
+    public Queue normalQueue() {
+        return QueueBuilder.durable(NORMAL_QUEUE)
+                .deadLetterExchange(DEAD_EXCHANGE)
+                .deadLetterRoutingKey(DEAD_ROUTING_KEY)
+                .build();
+    }
+
+    @Bean
+    public Binding normalBinding(TopicExchange normalExchange, Queue normalQueue) {
+        return BindingBuilder.bind(normalQueue).to(normalExchange).with(NORMAL_ROUTING_KEY);
+    }
+
+    @Bean
+    public TopicExchange deadExchange() {
+        return ExchangeBuilder.topicExchange(DEAD_EXCHANGE).build();
+    }
+
+    @Bean
+    public Queue deadQueue() {
+        return QueueBuilder.durable(DEAD_QUEUE).build();
+    }
+
+    @Bean
+    public Binding deadBinding(TopicExchange deadExchange, Queue deadQueue) {
+        return BindingBuilder.bind(deadQueue).to(deadExchange).with(DEAD_ROUTING_KEY);
+    }
+
+}
